7c7690d506753980d05ac181f8dfd67471443160
hecmed <hector.med27@gmail.com>
2019-07-22 22:09:13 -0700

    deleted unused code

--- example/client.py
+++ example/client.py
@@ -8,50 +8,6 @@ import os
 START = 0x01
 END = 0x00
 
-
-def connectEpheremal(dataPort):
-   
-   dataSocket = socket(AF_INET, SOCK_STREAM)
-   dataSocket.connect(('', dataPort))
-   conn = True
-   while conn:
-           conn = serverHandler(dataSocket, dataPort)
-
-   return
-
-def cmdList(clientSocket):
-    clientSocket.send(b'\x01\x05\x00')
-    return
-
-def parseMessage(buffer):
-    # Command format:
-    #  0     n-bytes      n+1 
-    # SoM   Data Length  EoM
-    # 0x01 ...........  0x00
-
-    print(buffer)
-    if buffer[1] == b'\x02':
-        #once port is open acknowledge connection?
-        print("Data port opened")
-        return
-
-def serverHandler(serverSocket, serverAddress):
-    print(f"Client connected from {serverAddress}")
-    
-    buffer = []
-    while 1:
-            newByte = serverSocket.recv(1)
-            buffer.append(newByte)
-            if newByte == b"\x01":
-                # Got start of message, reset the buffer
-                print("Byte came in")
-                buffer = [newByte]
-            elif newByte == b"\x00":
-                # Got end of message, parse it
-                parseMessage(buffer)
-                buffer = []
-                return False
-
 def getPortNumber(data):
     """
     Given a 0x05 packet in the data argument
